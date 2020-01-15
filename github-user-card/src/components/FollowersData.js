89fa9735e737e2d1d77a6dda68a937aef08a9f8b
Christina Kaliora <develop@pirron.one>
2020-01-15 19:53:01 +0100

    small changes

--- github-user-card/src/components/FollowersData.js
+++ github-user-card/src/components/FollowersData.js
@@ -6,15 +6,11 @@ import {
 
 function FollowersData(props){
     return (
-            <Card>
+            <Card style={{marginTop: "5%"}}>
                 <CardBody>
                     <CardSubtitle><h2>Username: {props.username}</h2></CardSubtitle>
                 </CardBody>
                 <img width="20%" src={props.img} alt="Card image cap" className="user-img"/>
-                <CardBody>
-                    <CardText>Location: {props.location}</CardText>
-                    <CardText>Followers: {props.followers}</CardText>
-                </CardBody>
             </Card>
         // <div>
         //     <h3>{props.username}</h3>
